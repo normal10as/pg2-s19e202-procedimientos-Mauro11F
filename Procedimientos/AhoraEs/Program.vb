1c1741c153ac577593a36aa9700ccfa4011a86dd
Mauro <Mauro@LAPTOP-IOFACOLC>
2019-05-20 16:28:06 -0300

    AhoraEs....

---
+++ Procedimientos/AhoraEs/Program.vb
@@ -0,0 +1,34 @@
+Imports System
+
+Imports System.DateTime
+
+'Crear un proyecto y un módulo 
+'llamado “AhoraEs” que imprima la fecha y 
+'hora actual con sus respectivas leyendas 
+'según los siguientes requerimientos  
+'• Utilizar una sub para imprimir la fecha actual. 
+'• Utilizar una function para obtener la hora actual. 
+'• Llamar a los procedimientos desde el Main.
+
+Module AhoraEs
+
+    Sub Main(args As String())
+
+        Fecha()
+        Console.WriteLine("La Hora actual es: " & Hora())
+
+
+    End Sub
+
+    Sub Fecha()
+
+        Console.WriteLine("Fecha actual: " & Now.Date)
+
+    End Sub
+
+    Function Hora()
+
+        Return (Now.Hour & ":" & Now.Minute & ":" & Now.Second)
+
+    End Function
+End Module
